e88ec4e0af3f75531239d74188ba22ae350a8270
Serzh108 <serzh108@gmail.com>
2020-04-17 13:04:18 +0300

    add index.js corrections

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './stylesheet/main.css';
 import './components/buttons/buttons';
+import './components/card/card';
+import './components/contacts/contacts';
+import './components/detalies/detalies';
+import './components/footer/footer';
+import './components/form/form';
+import './components/header/header';
+import './components/localStorage/localStorage'
+import './components/pagination/pagination';
 

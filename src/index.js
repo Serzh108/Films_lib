931b542b523ea23ddcf129c665d3dc0638d4310b
Bogdan <mormul96@ukr.net>
2020-04-20 17:29:01 +0300

    build footer request

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import './stylesheet/main.css';
 import './components/buttons/buttons';
 import './components/card/card';
 import './components/contacts/contacts';
-import './components/detalies/detalies';
+import './components/details/details';
 import './components/footer/footer';
 import './components/form/form';
 import './components/header/header';
-import './components/localStorage/localStorage'
+import './components/localStorage/localStorage';
 import './components/pagination/pagination';
-

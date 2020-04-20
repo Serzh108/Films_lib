9dcbb9877f4a48876ab084081665b38e348964ca
Ghore <ghoreua@gmail.com>
2020-04-20 18:32:34 +0300

    full CSS

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import './components/details/details';
 import './components/footer/footer';
 import './components/form/form';
 import './components/header/header';
-import './components/localStorage/localStorage'
+import './components/localStorage/localStorage';
 import './components/pagination/pagination';
 
 // TIMUR
 
-import './components/pagination/pagination.css'
+import './components/pagination/pagination.css';
+
+// Illia
+import './components/card/card.css';
 

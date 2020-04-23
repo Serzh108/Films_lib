350988b847d19b73e5850cb745ef0425947f9802
Bogdan <mormul96@ukr.net>
2020-04-23 11:08:14 +0300

    save footer

--- src/index.js
+++ src/index.js
@@ -4,21 +4,15 @@ import './components/buttons/buttons';
 import './components/contacts/contacts';
 import './components/details/details';
 import './components/footer/footer';
+import './components/footer/contact';
 import './components/form/form';
 import './components/header/header';
 import './components/localStorage/localStorage';
 import './components/pagination/pagination';
 
-
-
-
-
-
-
 // ----------- TIMUR
 import movies from './services/services';
-import './components/pagination/pagination.css'
-
+import './components/pagination/pagination.css';
 
 // sortGeneres();
 
@@ -42,7 +36,7 @@ import './components/pagination/pagination.css'
 //             },[]);
 //             console.log(finalGenres2);
 //         })
-        
+
 //     })
 // }
 
@@ -50,8 +44,6 @@ import './components/pagination/pagination.css'
 //     return arr.filter(elem => elem.id === id);
 // };
 
-
 // sortGeneres();
-
 
 // ----------------------------
